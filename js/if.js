f513faa24b58c2f8f4bead796d3be9d989702563
Felipe Monari <133614580+Monari14@users.noreply.github.com>
2025-03-26 20:00:29 -0300

    feat: atividade 2

---
+++ js/if.js
@@ -0,0 +1,17 @@
+let numero = 17;
+console.log(
+    "Número: " + numero
+);
+
+if(numero > 0 ) 
+{
+ console.log("Número é positivo.");   
+}
+else if (numero === 0) 
+{
+    console.log("Número é zero.");
+}
+else
+{
+    console.log("Número é negativo.");
+}
---
+++ js/if2.js
@@ -0,0 +1,15 @@
+let idade = 90;
+let possuirCarteira = true;
+
+console.log(
+    "\n Idade: " + idade +
+    "\n Possui Carteira?: " + possuirCarteira
+);
+if(idade >= 18 && possuirCarteira) 
+{
+    console.log("Você pode dirigir");
+}
+else 
+{
+    console.log("Você não pode dirigir!");
+}
