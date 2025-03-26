20372dbf6391fd68c1a1e15a138b220b021f13d3
Felipe Monari <133614580+Monari14@users.noreply.github.com>
2025-03-26 19:47:21 -0300

    feat: atividade 1

---
+++ js/script.js
@@ -0,0 +1,18 @@
+//variaveis
+let idade = 17;
+let temperatura = 14.69;
+let nome = "Felipe Eduardo Monari";
+let saudacao = "Hello Mundo!";
+let ativo = true;
+let falso = false;
+let resultado = null;
+
+console.log(
+    "\n Idade: " + idade + 
+    "\n Tempeatura: " + temperatura +
+    "\n Nome: " + nome + 
+    "\n Saudação: " + saudacao + 
+    "\n Ativo: " + ativo + 
+    "\n Falso: " + falso + 
+    "\n Resultado: " + resultado
+);
