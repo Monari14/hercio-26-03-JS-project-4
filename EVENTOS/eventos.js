fbf4212a44406cef2a3c5ea1bab8e28737c9897b
Felipe Monari <133614580+Monari14@users.noreply.github.com>
2025-04-02 20:08:26 -0300

    style: identação do js

--- EVENTOS/eventos.js
+++ EVENTOS/eventos.js
@@ -1,18 +1,20 @@
-
 // PEGA OS VALORES DOS INPUT QUANDO CLICA
 // NO BOTÃO E SETA NO PARÁGRAFO "alterar"
 const botao = document.getElementById("botao");
-botao.addEventListener("click", () => {
-    // get the valores thos inputs
-    const nome = document.getElementById("nome").value;
-    const email = document.getElementById("email").value;
-    // alert of foi cliqued
-    alert("Botão foi clicado!");
-    // get the valor of paragrafo
-    const alterar = document.getElementById("alterar");
-    // set the variabous no paragrifo
-    alterar.innerHTML = `${nome} | ${email}`;
-});
+botao.addEventListener(
+    "click", () => 
+    {
+        // get the valores thos inputs
+        const nome = document.getElementById("nome").value;
+        const email = document.getElementById("email").value;
+        // alert of foi cliqued
+        alert("Botão foi clicado!");
+        // get the valor of paragrafo
+        const alterar = document.getElementById("alterar");
+        // set the variabous no paragrifo
+        alterar.innerHTML = `${nome} | ${email}`;
+    }
+);
 
 /* SERVE PRA PEGAR AUTOMATICAMENTE O TEXTO (LETRA POR LETRA)
 const campoTexto = document.getElementById("nome");
